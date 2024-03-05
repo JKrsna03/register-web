d649a4bf5643df6c4ed580c3acb0e044f0a159ea
JKrsna03 <jktipsie0315@gmail.com>
2024-03-06 06:57:50 +0800

    added new file

---
+++ register.js
@@ -0,0 +1,32 @@
+console.log("Hello World")
+console.error("Error Happened!!")
+console.warn("Warning")
+
+//variables
+//string = words
+//number = 123
+//boolean = true or false
+//object = ["apple,"banana"]
+
+//typeof
+console.log(typeof(true))
+
+//declaration of variables
+let name = "John Krsna"; //string
+    lastname = `Tipones` //string
+    age = 20; //number
+    isStudent = true; //boolean
+    hobbies =["idk", "i can comment anything bruh"] //object
+
+    //function
+    run = function() 
+    {
+        console.log("Im Running")
+    };
+
+console.log(name,age,isStudent,hobbies, run)
+console.log(typeof run)
+console.table(hobbies)
+console.log("My name is "+ name + " " + lastname)
+console.log(`My name is ${name} ${lastname}`)
+
